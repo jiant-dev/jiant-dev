1fca88a1076077e00dfbb182d74d049160f03c46
Jesse Swanson <js11133@nyu.edu>
2020-07-15 17:21:12 -0700

    refactored benchmark task lists in data downloader. added support for downloading benchmarks and listing supported tasks.

--- jiant/scripts/download_data/runscript.py
+++ jiant/scripts/download_data/runscript.py
@@ -1,101 +1,90 @@
 import os
+import argparse
 
 import jiant.utils.python.io as py_io
-import jiant.utils.zconf as zconf
 import jiant.scripts.download_data.datasets.nlp_tasks as nlp_tasks_download
 import jiant.scripts.download_data.datasets.xtreme as xtreme_download
 import jiant.scripts.download_data.datasets.files_tasks as files_tasks_download
+from jiant.tasks.constants import GLUE_TASKS, SUPERGLUE_TASKS, XTREME_TASKS, BENCHMARKS
+
+NLP_DOWNLOADER_TASKS = GLUE_TASKS | SUPERGLUE_TASKS
+SUPPORTED_TASKS = NLP_DOWNLOADER_TASKS | XTREME_TASKS | {"squad_v1", "squad_v2"}
 
 
-@zconf.run_config
-class RunConfiguration(zconf.RunConfig):
-    output_base_path = zconf.attr(type=str)
-    task_name_ls = zconf.attr(type=str, default=None)
-
-    def _post_init(self):
-        if isinstance(self.task_name_ls, str):
-            self.task_name_ls = self.task_name_ls.split(",")
+def list_supported_tasks(args):
+    print("Supported tasks:")
+    for task in SUPPORTED_TASKS:
+        print(task)
 
 
-def download_data_and_write_config(
-    task_name: str, task_data_base_path: str, task_config_base_path: str
-):
-    os.makedirs(task_data_base_path, exist_ok=True)
-    os.makedirs(task_config_base_path, exist_ok=True)
-    if task_name in [
-        # GLUE
-        "cola",
-        "sst",
-        "mrpc",
-        "qqp",
-        "stsb",
-        "mnli",
-        "mnli_mismatched",
-        "qnli",
-        "rte",
-        "wnli",
-        "glue_diagnostics",
-        # SuperGLUE
-        "cb",
-        "copa",
-        "multirc",
-        "wic",
-        "wsc",
-        "boolq",
-        "record",
-        "superglue_broadcoverage_diagnostics",
-        "superglue_winogender_diagnostics",
-        # Other
-        "snli",
-    ]:
-        nlp_tasks_download.download_data_and_write_config(
-            task_name=task_name,
-            task_data_path=os.path.join(task_data_base_path, task_name),
-            task_config_path=os.path.join(task_config_base_path, f"{task_name}_config.json"),
-        )
-    elif task_name in [
-        "xnli",
-        "pawsx",
-        "udpos",
-        "panx",
-        "xquad",
-        "mlqa",
-        "tydiqa",
-        "bucc2018",
-        "tatoeba",
-    ]:
-        xtreme_download.download_xtreme_data_and_write_config(
-            task_name=task_name,
-            task_data_base_path=task_data_base_path,
-            task_config_base_path=task_config_base_path,
-        )
-    elif task_name == "squad_v1":
-        files_tasks_download.download_squad_v1_data_and_write_config(
-            task_data_path=os.path.join(task_data_base_path, task_name),
-            task_config_path=os.path.join(task_config_base_path, f"{task_name}.json"),
-        )
-    elif task_name == "squad_v2":
-        files_tasks_download.download_squad_v2_data_and_write_config(
-            task_data_path=os.path.join(task_data_base_path, task_name),
-            task_config_path=os.path.join(task_config_base_path, f"{task_name}.json"),
-        )
-    else:
-        raise KeyError(task_name)
+def download_data(args):
+    output_base_path = args.output_path
+    if args.tasks:
+        task_names = args.tasks
+    elif args.benchmark:
+        task_names = BENCHMARKS[args.benchmark]
 
+    task_data_base_path = py_io.create_dir(output_base_path, "data")
+    task_config_base_path = py_io.create_dir(output_base_path, "configs")
 
-def download_all_data(output_base_path: str, task_name_ls: list, verbose=True):
-    for i, task_name in enumerate(task_name_ls):
-        download_data_and_write_config(
-            task_name=task_name,
-            task_data_base_path=py_io.create_dir(output_base_path, "data"),
-            task_config_base_path=py_io.create_dir(output_base_path, "configs"),
-        )
-        if verbose:
-            print(f"Downloaded '{task_name}' ({i}/{len(task_name_ls)})")
+    assert set(task_names).issubset(SUPPORTED_TASKS)
+
+    # Download specified tasks and generate configs for specified tasks
+    for i, task_name in enumerate(task_names):
+        error_flag = False
+        task_data_path = os.path.join(task_data_base_path, task_name)
+
+        if task_name in NLP_DOWNLOADER_TASKS:
+            nlp_tasks_download.download_data_and_write_config(
+                task_name=task_name,
+                task_data_path=task_data_path,
+                task_config_path=os.path.join(task_config_base_path, f"{task_name}_config.json"),
+            )
+        elif task_name in XTREME_TASKS:
+            try:
+                xtreme_download.download_xtreme_data_and_write_config(
+                    task_name=task_name,
+                    task_data_base_path=task_data_base_path,
+                    task_config_base_path=task_config_base_path,
+                )
+            except NotImplementedError:
+                print("ERROR: " + task_name + " not implemented yet")
+                error_flag = True
+        elif task_name == "squad_v1":
+            files_tasks_download.download_squad_v1_data_and_write_config(
+                task_name=task_name,
+                task_data_path=task_data_path,
+                task_config_path=os.path.join(task_config_base_path, f"{task_name}.json"),
+            )
+        elif task_name == "squad_v2":
+            files_tasks_download.download_squad_v2_data_and_write_config(
+                task_name=task_name,
+                task_data_path=task_data_path,
+                task_config_path=os.path.join(task_config_base_path, f"{task_name}.json"),
+            )
+        if not error_flag:
+            print(f"Downloaded and generated configs for '{task_name}' ({i}/{len(task_names)})")
 
 
 def main():
-    args = RunConfiguration.default_run_cli()
-    download_all_data(
-        output_base_path=args.output_base_path, task_name_ls=args.task_name_ls, verbose=True,
+    parser = argparse.ArgumentParser(description="Download NLP datasets and generate task configs")
+    subparsers = parser.add_subparsers()
+    sp_list = subparsers.add_parser("list", help="list supported tasks in downloader")
+    sp_download = subparsers.add_parser("download", help="download data command")
+    sp_download.add_argument(
+        "--output_path", required=True, help="base output path for downloaded data and task configs"
     )
+    sp_download_group = sp_download.add_mutually_exclusive_group(required=True)
+    sp_download_group.add_argument("--tasks", nargs="+", help="list of tasks to download")
+    sp_download_group.add_argument("--benchmark", choices=BENCHMARKS)
+
+    # Hook subparsers up to functions
+    sp_list.set_defaults(func=list_supported_tasks)
+    sp_download.set_defaults(func=download_data)
+
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
