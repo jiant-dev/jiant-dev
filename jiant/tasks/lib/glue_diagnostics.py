3f4d3e1902175abd59f978c6829d1c02aea0679f
Jesse Swanson <js11133@nyu.edu>
2020-07-21 18:42:37 -0400

    added missing dataclass field for GLUE diagnostics task

--- jiant/tasks/lib/glue_diagnostics.py
+++ jiant/tasks/lib/glue_diagnostics.py
@@ -1,18 +1,24 @@
+from dataclasses import dataclass
+
 import jiant.tasks.lib.mnli as mnli
 
 
+@dataclass
 class Example(mnli.Example):
     pass
 
 
+@dataclass
 class TokenizedExample(mnli.TokenizedExample):
     pass
 
 
+@dataclass
 class DataRow(mnli.DataRow):
     pass
 
 
+@dataclass
 class Batch(mnli.Batch):
     pass
 
