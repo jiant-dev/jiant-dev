f7f83a0881c5097cb4c0252c659bda2317710be8
Jesse Swanson <js11133@nyu.edu>
2020-08-28 15:52:24 -0700

    added gpu runscript test

--- tests/proj/simple/test_runscript.py
+++ tests/proj/simple/test_runscript.py
@@ -22,3 +22,24 @@ def test_simple_runscript(tmpdir, task_name, model_type):
 
 	run_args = run.RunConfiguration.default_run_cli(cl_args=cl_args)
 	run.run_simple(run_args)
+
+@pytest.mark.gpu
+@pytest.mark.slow
+@pytest.mark.parametrize("task_name", downloader.SUPPORTED_TASKS)
+@pytest.mark.parametrize("model_type", ["roberta-large"])
+def test_simple_runscript(tmpdir, task_name, model_type):
+	data_dir = str(tmpdir.mkdir('data'))
+	exp_dir = str(tmpdir.mkdir('exp'))
+
+	downloader.download_data([task_name], data_dir)
+
+	cl_args = ["--run_name", task_name,
+	           "--exp_dir", exp_dir,
+	           "--data_dir", data_dir,
+	           "--model_type", model_type,
+	           "--tasks", task_name,
+	           "--train_examples_cap", "16",
+	           "--train_batch_size", "16"]
+
+	run_args = run.RunConfiguration.default_run_cli(cl_args=cl_args)
+	run.run_simple(run_args)
