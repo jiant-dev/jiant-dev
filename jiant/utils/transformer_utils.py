933506f3ab53cca67b4b6a07e4da1687a1bf4277
Jason Phang <email@jasonphang.com>
2020-09-16 03:01:10 -0400

    bart

--- jiant/utils/transformer_utils.py
+++ jiant/utils/transformer_utils.py
@@ -6,10 +6,7 @@ from jiant.shared.model_resolution import ModelArchitectures
 @contextlib.contextmanager
 def output_hidden_states_context(encoder):
     model_arch = ModelArchitectures.from_encoder(encoder)
-    if model_arch in (ModelArchitectures.BART, ModelArchitectures.MBART):
-        yield
-        return
-    elif model_arch in (
+    if model_arch in (
         ModelArchitectures.BERT,
         ModelArchitectures.ROBERTA,
         ModelArchitectures.ALBERT,
@@ -23,10 +20,12 @@ def output_hidden_states_context(encoder):
             modified_obj = encoder.encoder.config
         else:
             raise RuntimeError(f"Failed to convert model {type(encoder)} to output hidden states")
+        old_value = modified_obj.output_hidden_states
+        modified_obj.output_hidden_states = True
+        yield
+        modified_obj.output_hidden_states = old_value
+    elif model_arch in (ModelArchitectures.BART, ModelArchitectures.MBART):
+        yield
+        return
     else:
         raise KeyError(model_arch)
-
-    old_value = modified_obj.output_hidden_states
-    modified_obj.output_hidden_states = True
-    yield
-    modified_obj.output_hidden_states = old_value
